89145a04da8af84bdc6d8c2357e9846b437efe96
StepanBandera-droid <ribinstepan10@mail.ru>
2025-08-04 21:37:43 +0300

    fix: Prettier use is completed

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -4,39 +4,39 @@ import RegistrationPage from "@pages/RegistrationPage";
 import { type RouteObject } from "react-router-dom";
 
 export interface IRouterPAth {
-  id: string;
-  path: string;
-  label?: string;
+    id: string;
+    path: string;
+    label?: string;
 }
 
 export const paths = {
-  HOME: {
-    id: "home",
-    path: "/",
-  },
-  MESSAGE: {
-    id: "message",
-    path: "/chat",
-  },
-  REGISTRATION: {
-    id: "registration",
-    path: "/registration",
-  },
+    HOME: {
+        id: "home",
+        path: "/",
+    },
+    MESSAGE: {
+        id: "message",
+        path: "/chat",
+    },
+    REGISTRATION: {
+        id: "registration",
+        path: "/registration",
+    },
 };
 
 const routes: RouteObject[] = [
-  {
-    ...paths.HOME,
-    element: <HomePage />,
-  },
-  {
-    ...paths.MESSAGE,
-    element: <MessagePage />,
-  },
-  {
-    ...paths.REGISTRATION,
-    element: <RegistrationPage />,
-  },
+    {
+        ...paths.HOME,
+        element: <HomePage />,
+    },
+    {
+        ...paths.MESSAGE,
+        element: <MessagePage />,
+    },
+    {
+        ...paths.REGISTRATION,
+        element: <RegistrationPage />,
+    },
 ];
 
 export default routes;
