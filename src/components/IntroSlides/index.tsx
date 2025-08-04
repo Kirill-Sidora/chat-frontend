fc8ce6594fef0b045cbf7951cdc5942b7645f1d4
StepanBandera-droid <ribinstepan10@mail.ru>
2025-08-04 14:35:37 +0300

    fix:  WebSocket has been modified

--- src/components/IntroSlides/index.tsx
+++ src/components/IntroSlides/index.tsx
@@ -3,23 +3,23 @@ import { type ReactElement } from "react";
 import "./style.css";
 
 const IntroSlides = (): ReactElement => {
-    return (
-        <div className="home-page-intro-slides-wrapper">
-            {introSlidesImagesData.map((image) => (
-                <div
-                    className="home-page-intro-slide"
-                    key={image.id}
-                    style={{ left: image.left, top: image.top }}
-                >
-                    <img
-                        src={image.src}
-                        height={image.height}
-                        z-index={image.zIndex}
-                    ></img>
-                </div>
-            ))}
+  return (
+    <div className="home-page-intro-slides-wrapper">
+      {introSlidesImagesData.map((image) => (
+        <div
+          className="home-page-intro-slide"
+          key={image.id}
+          style={{ left: image.left, top: image.top }}
+        >
+          <img
+            src={image.src}
+            height={image.height}
+            z-index={image.zIndex}
+          ></img>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default IntroSlides;
--- src/pages/MessagePage/index.tsx
+++ src/pages/MessagePage/index.tsx
@@ -2,121 +2,142 @@ import Message from "@components/Message";
 import MessageInput from "@components/MessageInput";
 import { useState, useRef, useEffect } from "react";
 import { type IMessage } from "@app-types/message";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./style.css";
 
 function getRandomId() {
-    return (
-        Date.now().toString(36) +
-        Math.random().toString(36).substring(2).toString()
-    );
+  return (
+    Date.now().toString(36) + Math.random().toString(36).substring(2).toString()
+  );
 }
 
 export default function MessagePage() {
-    const [secondUsername, setSecondUsername] = useState<string | null>(null);
-    const [messages, setMessages] = useState<IMessage[]>([]);
+  const [secondUsername, setSecondUsername] = useState<string | null>(null);
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const location = useLocation();
 
-    const [ws, setWs] = useState<WebSocket | null>(null);
+  const wsRef = useRef<WebSocket | null>(null);
 
-    const messagesEndRef = useRef<HTMLDivElement>(null);
+  const messagesEndRef = useRef<HTMLDivElement>(null);
 
-    const username = localStorage.getItem("nickName");
+  const username = localStorage.getItem("nickName");
 
-    useEffect(() => {
-        const socket = new WebSocket("ws://localhost:3001");
+  useEffect(() => {
+    const isChatPage = location.pathname === "/chat";
 
-        socket.onopen = () => {
-            console.log("Connected to ws");
+    if (isChatPage) {
+      const socket = new WebSocket("ws://localhost:3001");
+      socket.onopen = () => {
+        console.log("Connected to ws");
 
-            socket.send(
-                JSON.stringify({ type: "init", username, id: getRandomId() })
-            );
-        };
+        socket.send(
+          JSON.stringify({ type: "init", username, id: getRandomId() })
+        );
 
-        socket.onmessage = (event) => {
-            const data = JSON.parse(event.data);
+        sendSystemMessage("USER_JOINED");
+      };
 
-            console.log("MESSAGE FROM SERVER: ", data);
+      socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
 
-            // if (data.type === "init" && data.username !== username) {
-            //     setSecondUser(data.username);
-            // }
+        console.log("MESSAGE FROM SERVER: ", data);
 
-            if (data.type === "msg") {
-                if (
-                    !secondUsername &&
-                    data.username !== localStorage.getItem("nickName")
-                ) {
-                    setSecondUsername(data.username);
+        // if (data.type === "init" && data.username !== username) {
+        //     setSecondUser(data.username);
+        // }
 
-                    console.log("SECOND USERNAME CHANGED: ", data.username);
-                }
-                const newMessage: IMessage = {
-                    id: Date.now().toString(),
-                    text: data.text,
-                    time: new Date().toLocaleTimeString([], {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                    }),
-                    isMine: data.username === localStorage.getItem("nickName"),
-                    sender: data.username,
-                };
-                if (localStorage.getItem("nickName") !== data.username) {
-                    setMessages((prev) => [...prev, newMessage]);
-                }
-            }
-        };
+        if (data.type === "msg") {
+          if (
+            !secondUsername &&
+            data.username !== localStorage.getItem("nickName")
+          ) {
+            setSecondUsername(data.username);
 
-        setWs(socket);
-
-        return;
-    }, []);
-
-    const handleSendMessage = (text: string) => {
-        const newMessage: IMessage = {
+            console.log("SECOND USERNAME CHANGED: ", data.username);
+          }
+          const newMessage: IMessage = {
             id: Date.now().toString(),
-            text,
+            text: data.text,
             time: new Date().toLocaleTimeString([], {
-                hour: "2-digit",
-                minute: "2-digit",
+              hour: "2-digit",
+              minute: "2-digit",
             }),
-            isMine: true,
-        };
-
-        setMessages([...messages, newMessage]);
-
-        if (ws && ws.readyState === WebSocket.OPEN) {
-            ws.send(JSON.stringify({ type: "msg", text, sender: username }));
+            isMine: data.username === localStorage.getItem("nickName"),
+            sender: data.username,
+          };
+          if (localStorage.getItem("nickName") !== data.username) {
+            setMessages((prev) => [...prev, newMessage]);
+          }
         }
+      };
+
+      wsRef.current = socket;
+
+      return () => {
+        if (socket.readyState === WebSocket.OPEN) {
+          sendSystemMessage("USER_LEFT");
+        }
+      };
+    }
+  }, [location.pathname]);
+
+  const handleSendMessage = (text: string) => {
+    const newMessage: IMessage = {
+      id: Date.now().toString(),
+      text,
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+      isMine: true,
     };
 
-    useEffect(() => {
-        messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [messages]);
+    setMessages([...messages, newMessage]);
 
-    return (
-        <div className="chat-page">
-            <header className="chat-header">
-                <Link to="/" className="chat-back-button secondary-text">
-                    ← Chats
-                </Link>
-                <div className="second-user-name-title primary-text">
-                    {secondUsername ? secondUsername : "now you're alone"}
-                </div>
-                <img
-                    src="src/assets/images/user-icon.png"
-                    className="user-icon"
-                />
-            </header>
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      wsRef.current.send(
+        JSON.stringify({ type: "msg", text, sender: username })
+      );
+    }
+  };
 
-            <div className="messages-container secondary-text">
-                {messages.map((msg) => (
-                    <Message key={msg.id} message={msg} />
-                ))}
-                <div ref={messagesEndRef} />
-            </div>
+  const sendSystemMessage = (type: string) => {
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      wsRef.current.send(
+        JSON.stringify({
+          type,
+          userId: getRandomId(),
+          username: username,
+          timestamp: new Date().toISOString(),
+        })
+      );
+    }
+  };
 
-            <MessageInput onSend={handleSendMessage} />
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <div className="chat-page">
+      <header className="chat-header">
+        <Link to="/" className="chat-back-button secondary-text">
+          ← Chats
+        </Link>
+        <div className="second-user-name-title primary-text">
+          {secondUsername ? secondUsername : "now you're alone"}
         </div>
-    );
+        <img src="src/assets/images/user-icon.png" className="user-icon" />
+      </header>
+
+      <div className="messages-container secondary-text">
+        {messages.map((msg) => (
+          <Message key={msg.id} message={msg} />
+        ))}
+        <div ref={messagesEndRef} />
+      </div>
+
+      <MessageInput onSend={handleSendMessage} />
+    </div>
+  );
 }
