e6af673bc9bdec1c717b5d0ac1198a0c11f94198
RenatAllakhyarov <rembo.909@yandex.ru>
2025-08-12 15:52:10 +0300

    fix: Moved ModalWindow to TextMode

--- src/components/FileUploader/index.tsx
+++ src/components/FileUploader/index.tsx
@@ -1,61 +1,34 @@
 import IconButton from "@components/IconButton";
-import ModalWindow from "@components/ModalWindow";
 import { useFileUpload } from "@hooks/useFileUpload";
-import { Fragment, type ReactElement } from "react";
+import { useEffect, type ReactElement } from "react";
 import { IconIds } from "@utils/constants";
-import ModalWindowContent from "@components/ImageModalWindowContent";
-import type { IEncodedFileData } from "@app-types/file";
-import FileManager from "@services/FileManager";
 
 export interface IFileUploaderProps {
-    onFileSend: (fileData: IEncodedFileData) => void;
+    onFileSelected: (file: File | null) => void;
 }
 
-const FileUploader = ({ onFileSend }: IFileUploaderProps): ReactElement => {
-    const { file, clear, isFileUpload, setIsFileUpload, handleUploadClick } =
+const FileUploader = ({ onFileSelected }: IFileUploaderProps): ReactElement => {
+    const { file, clear, handleUploadClick } =
         useFileUpload({
             type: "file",
             accept: ".jpg, .jpeg, .png",
             multiple: false,
         });
 
-    const handleSend = async () => {
-        if (!isFileUpload || !file) return;
+    useEffect(() => {
+        if (!file) return;
 
-        try {
-            const blob = new Blob([file], { type: file.type });
+        onFileSelected(file);
 
-            const encodedFileData: IEncodedFileData =
-                await FileManager.blobToBase64Data(blob, file.name);
-
-            onFileSend(encodedFileData);
-        } catch (error) {
-            const currenError = error as Error;
-
-            console.error("Failed encode file: ", currenError);
-        } finally {
-            clear();
-        }
-    };
+       clear();
+    }, [file, onFileSelected, clear]);
 
     return (
-        <Fragment>
             <IconButton
                 iconSrc={IconIds.PAPERCLIP_ICON}
                 onClick={() => handleUploadClick()}
                 height="24px"
             />
-
-            {isFileUpload && (
-                <ModalWindow>
-                    <ModalWindowContent
-                        file={file!}
-                        onClose={() => setIsFileUpload(false)}
-                        onSend={handleSend}
-                    />
-                </ModalWindow>
-            )}
-        </Fragment>
     );
 };
 
--- src/components/TextMode/index.tsx
+++ src/components/TextMode/index.tsx
@@ -1,11 +1,14 @@
 import IconButton from "@components/IconButton";
 import MessageInput from "@components/MessageInput";
 import FileUploader from "@components/FileUploader";
-import type { ReactElement, KeyboardEvent } from "react";
+import { type ReactElement, type KeyboardEvent, useState } from "react";
 import { IconIds } from "@utils/constants";
 import type { IEncodedFileData } from "@app-types/file";
+import FileManager from "@services/FileManager";
+import ModalWindow from "@components/ModalWindow";
+import ImageModalWindowContent from "@components/ImageModalWindowContent";
 
-interface TextModeProps {
+interface ITextModeProps {
     message: string;
     setMessage: (msg: string) => void;
     onKeyDown: (e: KeyboardEvent<HTMLTextAreaElement>) => void;
@@ -17,10 +20,48 @@ const TextMode = ({
     setMessage,
     onKeyDown,
     onFileSend,
-}: TextModeProps): ReactElement => {
+}: ITextModeProps): ReactElement => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedFile, setSelectedFile] =useState<File | null>(null);
+    
+    const handleFileSelected = (file: File | null) => {
+        setSelectedFile(file);
+
+        setIsModalOpen(true);
+    };
+    
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setSelectedFile(null);
+    };
+
+    const handleFileSend = async () => {
+        if (!selectedFile) {
+            return;
+        }
+
+        try {
+            const blob = new Blob([selectedFile], { type: selectedFile.type });
+
+            const encodedFileData: IEncodedFileData =
+                await FileManager.blobToBase64Data(blob, selectedFile.name);
+
+            onFileSend(encodedFileData);
+        } catch (error) {
+            const currenError = error as Error;
+
+            console.error("Failed encode file: ", currenError);
+        } finally {
+            handleCloseModal();
+        }
+    } ;
+
     return (
         <>
-            <FileUploader onFileSend={onFileSend}/>
+            <FileUploader 
+                onFileSelected={handleFileSelected}
+            />
+
             <MessageInput
                 message={message}
                 setMessage={setMessage}
@@ -32,6 +73,16 @@ const TextMode = ({
                 onClick={() => {}}
                 height="24px"
             />
+
+            {isModalOpen && (
+                <ModalWindow>
+                    <ImageModalWindowContent 
+                        file={selectedFile!}
+                        onClose={handleCloseModal}
+                        onSend={handleFileSend}
+                    />
+                </ModalWindow>
+            )}
         </>
     );
 };
